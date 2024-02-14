da615b11b9a50ee45a7532f4d3b2f70b1ccda5f0
raandaa_ <158516251+raandaa@users.noreply.github.com>
2024-02-15 00:41:00 +0200

    Add files via upload

---
+++ TechTalkContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace reverse_engineering;
+
+public partial class TechTalkContext : DbContext
+{
+    public TechTalkContext()
+    {
+    }
+
+    public TechTalkContext(DbContextOptions<TechTalkContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data source = .; Initial catalog = TechTalk ; Integrated Security = SSPI; TrustServerCertificate = True ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC07797C8FF8");
+
+            entity.Property(e => e.EndAt).HasColumnType("datetime");
+            entity.Property(e => e.StartAt).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .HasForeignKey(d => d.SpeakerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__398D8EEE");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC077AC0E219");
+
+            entity.Property(e => e.FirstName).HasMaxLength(25);
+            entity.Property(e => e.LastName).HasMaxLength(25);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
